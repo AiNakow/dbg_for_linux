2054b575426fbebc5fa6742b6c78c6232ce9ada5
AiNakow <ainakowalice@outlook.com>
2025-05-07 17:19:42 +0800

    modified example

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+int add(int a, int b) 
+{
+    return a + b;
+}
+
 int main() 
 {
     int a = 10;
-    std::cerr << "this is " << a << std::endl;
+    int b = 20;
+    int c = add(a, b);
+    std::cerr << add(a, c) << std::endl;
     return 0;
 }
